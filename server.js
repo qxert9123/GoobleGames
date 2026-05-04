df5ce27e2697822a43fb1cd2b5c3b40a1ad85ba7
qxert9123 <wbird@oocs.org>
2026-05-04 14:33:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,47 +4,69 @@ const WebSocket = require("ws");
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
 
-app.get("/", (req, res) => {
-  res.send("Multiplayer Server Running 🚀");
+// 🔥 Proper WebSocket handling for Render
+const wss = new WebSocket.Server({ noServer: true });
+
+server.on("upgrade", (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit("connection", ws, request);
+  });
 });
 
 let players = {};
 
+function broadcast() {
+  const data = JSON.stringify({
+    type: "update",
+    players
+  });
+
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+}
+
 wss.on("connection", (ws) => {
-  const id = Math.random().toString(36).substr(2, 9);
+  const id = Math.random().toString(36).slice(2, 10);
 
-  players[id] = { x: 100, y: 100 };
+  players[id] = {
+    x: Math.random() * 700,
+    y: Math.random() * 400
+  };
 
-  ws.send(JSON.stringify({ type: "init", id, players }));
+  ws.send(JSON.stringify({
+    type: "init",
+    id,
+    players
+  }));
 
   broadcast();
 
   ws.on("message", (msg) => {
-    const data = JSON.parse(msg);
+    try {
+      const data = JSON.parse(msg);
 
-    if (data.type === "move") {
-      players[id] = data.pos;
-      broadcast();
-    }
+      if (data.type === "move") {
+        players[id] = data.pos;
+        broadcast();
+      }
+    } catch (e) {}
   });
 
   ws.on("close", () => {
     delete players[id];
     broadcast();
   });
+});
 
-  function broadcast() {
-    const msg = JSON.stringify({ type: "update", players });
-
-    wss.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(msg);
-      }
-    });
-  }
+app.get("/", (req, res) => {
+  res.send("GoobleGames Server Running 🚀");
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("Server running"));
+server.listen(PORT, () => {
+  console.log("Server running");
+});
